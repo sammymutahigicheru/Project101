17dccf841ed938a2bb3a99b3e9e3c9c50b48fb33
sammymutahigicheru <sammymutahi12@gmail.com>
2020-12-07 07:28:01 +0300

    create the array

--- src/Big0Notations/AsymptoticAnalysis.java
+++ src/Big0Notations/AsymptoticAnalysis.java
@@ -1,4 +1,17 @@
 package Big0Notations;
 
 public class AsymptoticAnalysis {
+    private int[] theArray;
+    private int itemsInArray = 0;
+    private int arraySize;
+    static long timeStarted;
+    static long endTime;
+    public AsymptoticAnalysis(int size){
+        arraySize = size;
+        theArray = new int[arraySize];
+    }
+    public static void main(String[] args) {
+
+    }
+
 }
