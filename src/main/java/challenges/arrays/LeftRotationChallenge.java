e88954c372cd1fbd4e60cc1dcf29d8d399f1cbc4
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-09 08:51:21 +0300

    modify left rotation challenge class

--- src/main/java/challenges/arrays/LeftRotationChallenge.java
+++ src/main/java/challenges/arrays/LeftRotationChallenge.java
@@ -7,5 +7,26 @@ package challenges.arrays;
 *
 * */
 public class LeftRotationChallenge {
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        //grab the size
+        int size = a.length;
+        //create array to store the index
+        int[] rotated_array = new int[size];
+        int rotate_index = d;
+        int i = 0; //
+        while(rotate_index < size){
+            rotated_array[i] = a[rotate_index];
+            i++;
+            rotate_index++;
+        }
+        rotate_index = 0;
+        while(rotate_index < d){
+            rotated_array[i] = a[rotate_index];
+            i++;
+            rotate_index++;
+        }
+        return rotated_array;
 
+    }
 }
