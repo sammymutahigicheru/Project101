a14b6d4af0b12402d1c31c5c4e0793a93b538be2
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-10 11:41:52 +0300

    fix bug

--- src/main/java/challenges/arrays/NewYearChaosChallenge.java
+++ src/main/java/challenges/arrays/NewYearChaosChallenge.java
@@ -7,6 +7,7 @@ public class NewYearChaosChallenge {
     * O(1) space complexity
     * */
     // Complete the minimumBribes function below.
+    // Complete the minimumBribes function below.
     static void minimumBribes(int[] q) {
         int swaps = 0;
         for(int i=q.length-1;i>=0;i--){
@@ -16,10 +17,10 @@ public class NewYearChaosChallenge {
                     q[i-1] = q[i];
                     q[i] = temp;
                     swaps++;
-                }else if((i-2)>=0 && q[i-2]==i+1){
+                }else if((i-2)>=0 && q[i-2]== i+1){
                     q[i-2] = q[i-1];
                     q[i-1]=q[i];
-                    q[i] = q[1-2];
+                    q[i] = q[i-2];
                     swaps += 2;
                 }else{
                     System.out.println("Too chaotic");
