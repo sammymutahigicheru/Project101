210dd5c769c67b239a8ebeb9a5a91a6ccb5cb3a1
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-11 08:33:23 +0300

    update with optimal solution

--- src/main/java/challenges/arrays/MinimumSwaps2.java
+++ src/main/java/challenges/arrays/MinimumSwaps2.java
@@ -11,16 +11,34 @@ For example, given the array [7 1 3 2 4 5 6]  we perform the following steps
 
 public class MinimumSwaps2 {
     static int minimumSwaps(int[] arr) {
+        int min_index=0;
+        int min = arr[0];
         int swaps = 0;
-        for(int i=arr.length-1;i>=0;i--){
-            for(int j=0;j<i;j++){
-                if(arr[j] > arr[j+1]){
-                    int temp = arr[j+1];
-                    arr[j+1] = arr[j];
-                    arr[j] = temp;
-                    swaps++;
-                }
+        //update min index and min value
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<min){
+                min_index = i;
+                min = arr[i];
             }
+        }
+        //swap as long as minimum index is not equal to 0
+        if(min_index != 0){
+            int temp = arr[0];
+            arr[0] = arr[min_index];
+            arr[min_index] = temp;
+            swaps++;
+        }
+        for(int j=1;j<arr.length-1;j++){
+            //calculate the position
+            int pos = arr[j]-arr[0];
+            while(arr[pos] != arr[j]){
+                int temp = arr[pos];
+                arr[pos] = arr[j];
+                arr[j] = temp;
+                swaps++;
+                pos = arr[j]-arr[0];
+            }
+            j = pos;
         }
         return swaps;
     }
