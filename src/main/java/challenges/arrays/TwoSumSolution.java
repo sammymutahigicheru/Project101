f3731755708f168e436557e18505ac4074b9b0f2
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-15 18:40:49 +0300

    optimize first solution using a hastable

--- src/main/java/challenges/arrays/TwoSumSolution.java
+++ src/main/java/challenges/arrays/TwoSumSolution.java
@@ -1,5 +1,8 @@
 package challenges.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
@@ -27,6 +30,27 @@ public class TwoSumSolution {
 
         }
         return indexes;
+
+    }
+   /*
+   * Solution 2
+   * Optimal Solution
+   * O(n) time complexity
+   * O(n) space complexity
+   *
+   * */
+    Map<Integer,Integer> map = new HashMap<>();
+    public int[] twoSumHashTable(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],i); //store all elements in the array
+        }
+        for(int i=0;i<nums.length;i++){
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) != i){ //lookup takes O(1) time complexity assuming there is no collision
+                return new int[]{i,map.get(complement)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
 
     }
 }
