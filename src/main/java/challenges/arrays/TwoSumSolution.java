3e368373ca33a0fc7b5b1316a16a7db0821e30c8
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-15 18:36:39 +0300

    add a bruteforce solution

--- src/main/java/challenges/arrays/TwoSumSolution.java
+++ src/main/java/challenges/arrays/TwoSumSolution.java
@@ -6,4 +6,27 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 * */
 public class TwoSumSolution {
+    /*
+     * A brute force solution
+     * Takes O(n^2) time complexity
+     * and
+     * O(1) space complexity
+     *
+     * */
+    int[] indexes = new int[2];
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) { //two pointer
+                if (nums[j] == target - nums[i]) {
+                    indexes[0] = i;
+                    indexes[1] = j;
+                }
+
+            }
+
+        }
+        return indexes;
+
+    }
 }
