fc828abeec2c2fbc8223fd70c18099c18dc1806f
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-16 08:04:45 +0300

    add problem statement

--- src/main/java/challenges/arrays/RemoveDuplicatesOfAGivenValue.java
+++ src/main/java/challenges/arrays/RemoveDuplicatesOfAGivenValue.java
@@ -1,4 +1,11 @@
 package challenges.arrays;
-
+/*
+* Remove duplicates of a given value element from the array
+*
+*
+* */
 public class RemoveDuplicatesOfAGivenValue {
+    public int removeElement(int[] nums,int val){
+
+    }
 }
