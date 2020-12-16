2e5a9b32322e32e08d07f619a400f79bfe0956ae
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-16 08:20:28 +0300

    add solution to the problem

--- src/main/java/challenges/arrays/FindDuplicates.java
+++ src/main/java/challenges/arrays/FindDuplicates.java
@@ -1,5 +1,6 @@
 package challenges.arrays;
 
+import java.util.HashSet;
 import java.util.Set;
 
 /*
@@ -7,7 +8,17 @@ import java.util.Set;
 *
 * */
 public class FindDuplicates {
+    Set<Integer> duplicates = new HashSet<>();
     public Set<Integer> findDuplicates(int[] input){
-
+        //a two pointer problem
+        for (int i=0;i<input.length;i++){
+            for(int j=1;j<input.length;j++){
+                if(input[i] == input[j] && i != j){
+                    duplicates.add(input[j]);
+                    break;
+                }
+            }
+        }
+        return duplicates;
     }
 }
