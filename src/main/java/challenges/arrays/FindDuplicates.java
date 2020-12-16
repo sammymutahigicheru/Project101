99bd34ec9471b0c303feda337143d4a0c75a2ede
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-16 08:16:31 +0300

    add find duplicates function

--- src/main/java/challenges/arrays/FindDuplicates.java
+++ src/main/java/challenges/arrays/FindDuplicates.java
@@ -1,8 +1,13 @@
 package challenges.arrays;
 
+import java.util.Set;
+
 /*
 * Find Duplicates in a given Array
 *
 * */
 public class FindDuplicates {
+    public Set<Integer> findDuplicates(int[] input){
+
+    }
 }
