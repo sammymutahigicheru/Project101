18b16ccc4e466cce345a68c971fb58cd32d4e449
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-17 08:45:48 +0300

    add problem statement

--- src/main/java/challenges/arrays/RemoveDuplicates.java
+++ src/main/java/challenges/arrays/RemoveDuplicates.java
@@ -1,11 +1,15 @@
 package challenges.arrays;
 
+/*
+* Remove All Duplicates From a given array
+*
+* */
 public class RemoveDuplicates {
-    public int removeAllDuplicates(int[] input){
+    public int removeAllDuplicates(int[] input) {
         int targetIndex = 0;
         int noDuplicates = input[1];
-        for (int i=1;i<input.length;i++){
-            if(input[i] != noDuplicates){
+        for (int i = 1; i < input.length; i++) {
+            if (input[i] != noDuplicates) {
                 noDuplicates = input[i];
                 input[targetIndex] = input[i];
                 targetIndex++;
