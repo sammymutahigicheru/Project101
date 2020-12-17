436295226d8bc6cd48b05e8d752377b44d63bfbe
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-17 09:38:47 +0300

    modify the method to print max and min values in the array

--- src/main/java/challenges/arrays/FindMaxMinValues.java
+++ src/main/java/challenges/arrays/FindMaxMinValues.java
@@ -3,7 +3,18 @@ package challenges.arrays;
 * Find max and Min Value in a given array
 * */
 public class FindMaxMinValues {
-    public void findMaxMinValue(int[] input){
-
+    static void findMaxMinValue(int[] input){
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for (int i=0;i<input.length;i++){
+            if(input[i]>max){
+                max = input[i];
+            }else if(input[i]<min){
+                min = input[i];
+            }
+        }
+        System.out.println("Maximum value is => "+max);
+        System.out.println("**************************");
+        System.out.println("Minimum value is => "+min);
     }
 }
