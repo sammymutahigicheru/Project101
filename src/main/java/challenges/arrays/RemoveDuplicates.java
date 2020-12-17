22c9a0552bd4204b40b535f97abe5e88dd17189a
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-17 08:43:04 +0300

    add removeAllDuplicates in array method

--- src/main/java/challenges/arrays/RemoveDuplicates.java
+++ src/main/java/challenges/arrays/RemoveDuplicates.java
@@ -1,4 +1,16 @@
 package challenges.arrays;
 
 public class RemoveDuplicates {
+    public int removeAllDuplicates(int[] input){
+        int targetIndex = 0;
+        int noDuplicates = input[1];
+        for (int i=1;i<input.length;i++){
+            if(input[i] != noDuplicates){
+                noDuplicates = input[i];
+                input[targetIndex] = input[i];
+                targetIndex++;
+            }
+        }
+        return targetIndex;
+    }
 }
