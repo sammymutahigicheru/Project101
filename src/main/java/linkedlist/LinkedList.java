2cb7d2864f4b07769fadca8ae240cd491aa4deb7
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-23 10:31:25 +0300

    create Node class

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -1,4 +1,11 @@
 package linkedlist;
 
 public class LinkedList {
+    public static class Node{
+        int data;
+        Node node;
+        public Node(int data){
+            this.data = data;
+        }
+    }
 }
