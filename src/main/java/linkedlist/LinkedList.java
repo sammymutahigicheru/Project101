37bb18fb97f2abe99367d942275f85d08250f60e
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-23 10:35:43 +0300

    modify Node class

--- src/main/java/linkedlist/LinkedList.java
+++ src/main/java/linkedlist/LinkedList.java
@@ -3,7 +3,7 @@ package linkedlist;
 public class LinkedList {
     public static class Node{
         int data;
-        Node node;
+        Node next; //pointer
         public Node(int data){
             this.data = data;
         }
