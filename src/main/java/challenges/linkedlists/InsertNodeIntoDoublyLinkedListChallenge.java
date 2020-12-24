4ba106da0ad28008b192092d0cd1a0a64bdae44c
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-24 10:01:26 +0300

    solve the challenge

--- src/main/java/challenges/linkedlists/InsertNodeIntoDoublyLinkedListChallenge.java
+++ src/main/java/challenges/linkedlists/InsertNodeIntoDoublyLinkedListChallenge.java
@@ -10,5 +10,38 @@ class DoublyLinkedListNode {
 }
 
 public class InsertNodeIntoDoublyLinkedListChallenge {
+    static DoublyLinkedListNode sortedInsert(DoublyLinkedListNode head, int data) {
+        //create new node
+        DoublyLinkedListNode newNode = new DoublyLinkedListNode(data);
+        if(head == null){
+            return newNode;
+        }
+        //current node
+        DoublyLinkedListNode current = head;
+        DoublyLinkedListNode previous = null;
+        //first edge case: data < current.data
+        if(data < current.data){
+            newNode.next = current.next;
+            newNode.prev = null;
+            current.prev = newNode;
+            return newNode;
+        }
+        //move to the back of the list
+        if(current != null && data > current.data){
+            previous = current;
+            current = current.next;
+        }
+        if(current == null){
+            newNode.prev = previous;
+            newNode.next = null;
+            previous.next = newNode;
+        }else{
+            newNode.prev = previous;
+            newNode.next = previous.next;
+            previous.next = newNode;
+            newNode.next.prev = newNode;
+        }
 
+        return head;
+    }
 }
