ad35e6b3d1ceacb89c74ce75d6f040e8971f00df
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-24 09:59:23 +0300

    create DoublyLinkedListNode class

--- src/main/java/challenges/linkedlists/InsertNodeIntoDoublyLinkedListChallenge.java
+++ src/main/java/challenges/linkedlists/InsertNodeIntoDoublyLinkedListChallenge.java
@@ -1,4 +1,14 @@
 package challenges.linkedlists;
 
+class DoublyLinkedListNode {
+    int data;
+    DoublyLinkedListNode next;
+    DoublyLinkedListNode prev;
+    public DoublyLinkedListNode(int data){
+        this.data = data;
+    }
+}
+
 public class InsertNodeIntoDoublyLinkedListChallenge {
+
 }
