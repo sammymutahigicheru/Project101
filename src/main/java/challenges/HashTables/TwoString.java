4c5e78cfd45acb82efbbdd34ee9fc59f0e54fca4
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 14:28:30 +0300

    add a problem statement

--- src/main/java/challenges/HashTables/TwoString.java
+++ src/main/java/challenges/HashTables/TwoString.java
@@ -1,4 +1,7 @@
 package challenges.HashTables;
-
+/*
+* Given two strings, determine if they share a common substring.
+* A substring may be as small as one character.
+* */
 public class TwoString {
 }
