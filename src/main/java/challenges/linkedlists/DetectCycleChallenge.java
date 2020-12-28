05ccf3eda559137cf9ab4c5ecc51b622400b4ba3
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 10:41:56 +0300

    solve detect cycles in a linkedlist problem

--- src/main/java/challenges/linkedlists/DetectCycleChallenge.java
+++ src/main/java/challenges/linkedlists/DetectCycleChallenge.java
@@ -8,4 +8,22 @@ public class DetectCycleChallenge {
             this.data = data;
         }
     }
+
+    boolean hasCycle(Node head) {
+        if(head == null) return false;
+        //fast node
+        Node fast = head.next;
+        //slow node
+        Node slow = head;
+        while(fast != null && fast.next != null){
+            if(slow == fast){
+                return true;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        //traversed to the end without detecting a cycle
+        return false;
+    }
+
 }
