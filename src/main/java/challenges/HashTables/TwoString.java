2d3ef62a7a69678a4580282ef82eaddd937739dc
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 14:49:55 +0300

    modify two string method

--- src/main/java/challenges/HashTables/TwoString.java
+++ src/main/java/challenges/HashTables/TwoString.java
@@ -13,15 +13,13 @@ public class TwoString {
                 || (s2.length() > 100000)) {
             return "NO";
         }
-        HashMap<Character, Integer> hash_s1 = new HashMap();
-        for (int i = 0; i < s1.length(); i++) {
-            if (hash_s1.get(s2.charAt(i)) != null) {
-                if (hash_s1.containsKey(s2.charAt(i))) {
-                    return "YES";
-                }
-                hash_s1.put(s1.charAt(i), 1);
-            } else {
-                hash_s1.put(s1.charAt(i), 1);
+        HashMap<Character,Integer> hash_s1 = new HashMap();
+        for(int i=0;i<s1.length();i++){
+            hash_s1.put(s1.charAt(i),i);
+        }
+        for(int j=0;j<s2.length();j++){
+            if(hash_s1.containsKey(s2.charAt(j))){
+                return "YES";
             }
         }
         return "NO";
