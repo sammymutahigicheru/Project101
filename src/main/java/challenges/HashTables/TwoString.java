404f3a8ee90e292cf50afc73dbdbdf255bba6284
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 14:48:13 +0300

    handle constraints

--- src/main/java/challenges/HashTables/TwoString.java
+++ src/main/java/challenges/HashTables/TwoString.java
@@ -3,18 +3,25 @@ package challenges.HashTables;
 import java.util.HashMap;
 
 /*
-* Given two strings, determine if they share a common substring.
-* A substring may be as small as one character.
-* */
+ * Given two strings, determine if they share a common substring.
+ * A substring may be as small as one character.
+ * */
 public class TwoString {
     static String twoStrings(String s1, String s2) {
-        HashMap<Character,Integer> hash_s1 = new HashMap();
-        for(int i=0;i<s1.length();i++){
-            hash_s1.put(s1.charAt(i),i);
+        //constraints
+        if ((s1.length() < 1) || (s2.length() < 1) || (s1.length() > 100000)
+                || (s2.length() > 100000)) {
+            return "NO";
         }
-        for(int j=0;j<s2.length();j++){
-            if(hash_s1.containsKey(s2.charAt(j))){
-                return "YES";
+        HashMap<Character, Integer> hash_s1 = new HashMap();
+        for (int i = 0; i < s1.length(); i++) {
+            if (hash_s1.get(s2.charAt(i)) != null) {
+                if (hash_s1.containsKey(s2.charAt(i))) {
+                    return "YES";
+                }
+                hash_s1.put(s1.charAt(i), 1);
+            } else {
+                hash_s1.put(s1.charAt(i), 1);
             }
         }
         return "NO";
