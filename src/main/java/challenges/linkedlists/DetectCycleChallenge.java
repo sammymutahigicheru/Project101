4add2dc0df230b38a6a46752302ba8bc287d9838
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 10:40:08 +0300

    create Node Class

---
+++ src/main/java/challenges/linkedlists/DetectCycleChallenge.java
@@ -0,0 +1,11 @@
+package challenges.linkedlists;
+
+public class DetectCycleChallenge {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+        }
+    }
+}
