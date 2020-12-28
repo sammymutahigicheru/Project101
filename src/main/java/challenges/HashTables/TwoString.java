2dbaf5b3990704b00d4d3f6fab404fa42f09be40
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 14:29:56 +0300

    write a brute force solution for the problem

--- src/main/java/challenges/HashTables/TwoString.java
+++ src/main/java/challenges/HashTables/TwoString.java
@@ -1,7 +1,22 @@
 package challenges.HashTables;
+
+import java.util.HashMap;
+
 /*
 * Given two strings, determine if they share a common substring.
 * A substring may be as small as one character.
 * */
 public class TwoString {
+    static String twoStrings(String s1, String s2) {
+        HashMap<Character,Integer> hash_s1 = new HashMap();
+        for(int i=0;i<s1.length();i++){
+            hash_s1.put(s1.charAt(i),i);
+        }
+        for(int j=0;j<s2.length();j++){
+            if(hash_s1.containsKey(s2.charAt(j))){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
 }
