b24b305db8ff88afd271dffef0280ca6253aa66e
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-28 14:51:47 +0300

    add time complexity

--- src/main/java/challenges/HashTables/TwoString.java
+++ src/main/java/challenges/HashTables/TwoString.java
@@ -7,13 +7,17 @@ import java.util.HashMap;
  * A substring may be as small as one character.
  * */
 public class TwoString {
+    /*
+    * Takes O(m+n) time complexity
+    *
+    * */
     static String twoStrings(String s1, String s2) {
         //constraints
         if ((s1.length() < 1) || (s2.length() < 1) || (s1.length() > 100000)
                 || (s2.length() > 100000)) {
             return "NO";
         }
-        HashMap<Character,Integer> hash_s1 = new HashMap();
+        HashMap<Character, Integer> hash_s1 = new HashMap();
         for(int i=0;i<s1.length();i++){
             hash_s1.put(s1.charAt(i),i);
         }
