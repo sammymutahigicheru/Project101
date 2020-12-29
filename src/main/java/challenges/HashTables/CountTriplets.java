151f09bf2db6521d86d9cd93b5178a75027c0a46
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-29 12:30:18 +0300

    create countTriplets function

--- src/main/java/challenges/HashTables/CountTriplets.java
+++ src/main/java/challenges/HashTables/CountTriplets.java
@@ -1,7 +1,12 @@
 package challenges.HashTables;
+
+import java.util.List;
+
 /*
 * You are given an array and you need to find number of tripets of indices  such that the elements at those indices are in geometric progression for a given common ratio  and .
 * */
 public class CountTriplets {
+    static long countTriplets(List<Long> arr, long r){
 
+    }
 }
