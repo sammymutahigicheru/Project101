a6786f2acd19d3b3a69380daee95fc76db58d839
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-29 12:28:36 +0300

    create CountTriplets class and add problem statement

---
+++ src/main/java/challenges/HashTables/CountTriplets.java
@@ -0,0 +1,7 @@
+package challenges.HashTables;
+/*
+* You are given an array and you need to find number of tripets of indices  such that the elements at those indices are in geometric progression for a given common ratio  and .
+* */
+public class CountTriplets {
+
+}
