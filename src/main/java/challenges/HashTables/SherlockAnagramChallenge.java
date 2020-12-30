6c9df1a1241d553e5e95bfdf9034a001dab75efb
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-30 14:09:08 +0300

    add problem descritpion

--- src/main/java/challenges/HashTables/SherlockAnagramChallenge.java
+++ src/main/java/challenges/HashTables/SherlockAnagramChallenge.java
@@ -1,4 +1,8 @@
 package challenges.HashTables;
 
+/*
+*Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string.
+* Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+* */
 public class SherlockAnagramChallenge {
 }
