9cfd929a003889111bb16d5fb85a0d54137d0290
Sammy Mutahi <sammymutahi12@gmail.com>
2020-12-31 09:56:39 +0300

    add problem description

--- src/main/java/challenges/HashTables/FrequencyQueries.java
+++ src/main/java/challenges/HashTables/FrequencyQueries.java
@@ -1,4 +1,12 @@
 package challenges.HashTables;
+/*
+*You are given  queries. Each query is of the form two integers described below:
+- 1x : Insert x in your data structure.
+-  2y : Delete one occurence of y from your data structure, if present.
+- 3z : Check if any integer is present whose frequency is exactly z. If yes, print 1 else 0.
 
+The queries are given in the form of a 2-D array  of size  where  contains the operation, and  contains the data element. For example, you are given array
+*
+* */
 public class FrequencyQueries {
 }
