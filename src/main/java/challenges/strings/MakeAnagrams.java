f885c5cdd57c133680d4f4de680e0145a87e7898
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-07 08:57:08 +0300

    add problem statement

--- src/main/java/challenges/strings/MakeAnagrams.java
+++ src/main/java/challenges/strings/MakeAnagrams.java
@@ -1,4 +1,14 @@
 package challenges.strings;
 
+/*
+* A student is taking a cryptography class and has found anagrams to be very useful. Two strings are anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency. For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+The student decides on an encryption scheme that involves two large strings. The encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Determine this number.
+
+Given two strings, a  and b , that may or may not be of the same length, determine the minimum number of character deletions required to make  and  anagrams. Any characters can be deleted from either of the strings.
+*
+*
+* */
+
 public class MakeAnagrams {
 }
