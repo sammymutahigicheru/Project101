3dafe1dec485852686af342e8b7cba1bd187a1e5
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-07 20:54:43 +0300

    add problem set

--- src/main/java/challenges/strings/AlternatingCharacters.java
+++ src/main/java/challenges/strings/AlternatingCharacters.java
@@ -1,4 +1,11 @@
 package challenges.strings;
 
+/*
+* You are given a string containing characters A and B only.
+*  Your task is to change it into a string such that there are no matching adjacent characters.
+* To do this, you are allowed to delete zero or more characters in the string.
+* Your task is to find the minimum number of required deletions.
+*
+* */
 public class AlternatingCharacters {
 }
