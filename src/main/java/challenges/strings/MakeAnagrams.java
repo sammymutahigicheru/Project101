67cd49feecfa51bec2a14caf824c005849a2d1dc
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-07 08:59:56 +0300

    add make anagram solution

--- src/main/java/challenges/strings/MakeAnagrams.java
+++ src/main/java/challenges/strings/MakeAnagrams.java
@@ -10,8 +10,27 @@ Given two strings, a  and b , that may or may not be of the same length, determi
 *
 * */
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MakeAnagrams {
     static int makeAnagram(String a, String b){
+        HashMap<Character,Integer> count = new HashMap();
 
+        for(char ch: a.toCharArray()){
+            int ct = count.containsKey(ch)?count.get(ch) : 0;
+            count.put(ch,(ct + 1));
+        }
+        for(char ch: b.toCharArray()){
+            int ct = count.containsKey(ch) ? count.get(ch) : 0;
+            count.put(ch,(ct - 1));
+        }
+        List<Integer> value = new ArrayList(count.values());
+        int total = 0;
+        for(int v: value){
+            total += Math.abs(v);
+        }
+        return total;
     }
 }
