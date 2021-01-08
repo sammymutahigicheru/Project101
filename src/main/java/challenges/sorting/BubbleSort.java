1536752478c8de2c272b783d9da4a729d4f4b557
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-08 08:43:18 +0300

    modify count swaps method to return total swaps,first and last element of the sorted array

--- src/main/java/challenges/sorting/BubbleSort.java
+++ src/main/java/challenges/sorting/BubbleSort.java
@@ -4,6 +4,25 @@ package challenges.sorting;
 * */
 public class BubbleSort {
     static void countSwaps(int[] a) {
-
+        int swaps = 0;
+        for(int i=0;i<a.length;i++){
+            for(int j=1;j<a.length-i;j++){
+                if(a[j-1]>a[j]){
+                    int temp = a[j-1];
+                    a[j-1] = a[j];
+                    a[j]=temp;
+                    swaps++;
+                }
+            }
+        }
+        if(swaps>=1){
+            System.out.println("Array is sorted in "+swaps+" swaps.");
+            System.out.println("First Element: "+a[0]);
+            System.out.println("Last Element: "+a[a.length-1]);
+        }else{
+            System.out.println("Array is sorted in 0 swaps.");
+            System.out.println("First Element: "+a[0]);
+            System.out.println("Last Element: "+a[a.length-1]);
+        }
     }
 }
