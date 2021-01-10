dfae6240128bcfb1c2c6476deaa67936ac020382
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-10 12:38:51 +0300

    add problem statement

--- src/main/java/challenges/strings/SherlockAndValidString.java
+++ src/main/java/challenges/strings/SherlockAndValidString.java
@@ -2,6 +2,14 @@ package challenges.strings;
 
 import java.util.*;
 
+/*
+*
+* Sherlock considers a string to be valid if all characters of the string appear the same number of times.
+* It is also valid if he can remove just  character at  index in the string, and the remaining characters will occur the same number of times.
+* Given a string s, determine if it is valid. If so, return YES, otherwise return NO.
+*
+* */
+
 public class SherlockAndValidString {
     static String isValid(String s) {
         HashMap<Character, Integer> count = new HashMap();
