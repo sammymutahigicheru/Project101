1ca749a39afd46d76c58068bbc9f7c451a11571e
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-11 08:58:41 +0300

    add problem statement

--- src/main/java/challenges/strings/SpecialStringAgain.java
+++ src/main/java/challenges/strings/SpecialStringAgain.java
@@ -1,4 +1,14 @@
 package challenges.strings;
 
+/*
+* A string is said to be a special string if either of two conditions is met:
+
+All of the characters are the same, e.g. aaa.
+All characters except the middle one are the same, e.g. aadaa.
+A special substring is any substring of a string which meets one of those criteria.
+* Given a string, determine how many special substrings can be formed from it.
+*
+* */
+
 public class SpecialStringAgain {
 }
