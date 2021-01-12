b4149a508dfd31a83995bc55f0f83a57f1bc51fa
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-12 10:12:23 +0300

    add problem statement

--- src/main/java/challenges/strings/CommonChild.java
+++ src/main/java/challenges/strings/CommonChild.java
@@ -2,6 +2,15 @@ package challenges.strings;
 
 import java.util.HashMap;
 
+
+/*
+* A string is said to be a child of a another string if it can be formed by deleting 0 or more characters from the other string. Given two strings of equal length, what's the longest string that can be constructed such that it is a child of both?
+
+For example, ABCD and ABDC have two children with maximum length 3, ABC and ABD. They can be formed by eliminating either the D or C from both strings. Note that we will not consider ABCD as a common child because we can't rearrange characters and ABCD  ABDC.
+*
+*
+* */
+
 public class CommonChild {
     /*
     * Takes O(n*m) time complexity
