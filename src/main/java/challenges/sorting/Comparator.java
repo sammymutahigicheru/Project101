9c42194ca8b3dd9953552c5d23dcae138eb1066e
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-14 10:10:22 +0300

    add comparator solution

--- src/main/java/challenges/sorting/Comparator.java
+++ src/main/java/challenges/sorting/Comparator.java
@@ -1,4 +1,16 @@
 package challenges.sorting;
 
+class Player{
+    String name;
+    int score;
+}
+
 public class Comparator {
+
+    public static int compare(Player a,Player b){
+        if (a.score == b.score){
+            a.name.compareTo(b.name);
+        }
+        return b.score - a.score;
+    }
 }
