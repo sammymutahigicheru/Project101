3d039c5810b7689d48a9f3e049c30e783b6abe2a
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-14 10:12:42 +0300

    modify compare method

--- src/main/java/challenges/sorting/Comparator.java
+++ src/main/java/challenges/sorting/Comparator.java
@@ -9,7 +9,7 @@ public class Comparator {
 
     public static int compare(Player a,Player b){
         if (a.score == b.score){
-            a.name.compareTo(b.name);
+           return a.name.compareTo(b.name);
         }
         return b.score - a.score;
     }
