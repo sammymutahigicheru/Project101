f8ecfbf506f9866de0f0d0423ccb59eb17b03e15
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-14 10:14:46 +0300

    add time complexity

--- src/main/java/challenges/sorting/Comparator.java
+++ src/main/java/challenges/sorting/Comparator.java
@@ -6,7 +6,9 @@ class Player{
 }
 
 public class Comparator {
-
+    /*
+    * Takes O(1) time complexity
+    * */
     public static int compare(Player a,Player b){
         if (a.score == b.score){
            return a.name.compareTo(b.name);
