4da042e3b2bc7efd48131d5d34509d4c9b2ec9dd
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-19 09:15:37 +0300

    add problem statement

--- src/main/java/challenges/arrays/ReverseString.java
+++ src/main/java/challenges/arrays/ReverseString.java
@@ -1,23 +1,31 @@
 package challenges.arrays;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/*
+* Given an input string s, reverse the order of the words.
+
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+Return a string of the words in reverse order concatenated by a single space.
+
+Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+* */
 public class ReverseString {
     static String reverse(String s){
-        s=s.trim();
-        String []arr=s.split("\\s+");
-        String ans="";
-        for(String i:arr){
-            ans=i+" "+ans;
-            //System.out.println(ans);
+        String reversed = "";
+        String[] stringArray = s.trim().split("\\s+");
+        for (int i=stringArray.length-1;i>=0;i--){
+            reversed += stringArray[i]+ " ";
         }
-
-        return ans;
+        return reversed;
     }
 
     public static void main(String[] args) {
         String s = "a good   example";
-       System.out.println( reverse(s));
+        System.out.println(reverse(s));
     }
 }
