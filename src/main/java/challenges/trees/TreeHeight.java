29b6e7a27a18b9f33d32b844000ccc8c0b21fd42
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-20 14:56:02 +0300

    add insert method

--- src/main/java/challenges/trees/TreeHeight.java
+++ src/main/java/challenges/trees/TreeHeight.java
@@ -2,12 +2,29 @@ package challenges.trees;
 
 public class TreeHeight {
 
+
+
+    public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
 }
 class Node{
-    int value;
-    int left;
-    int right;
+    int data;
+    Node left;
+    Node right;
     Node(int value){
-        this.value = value;
+        this.data = value;
     }
 }
