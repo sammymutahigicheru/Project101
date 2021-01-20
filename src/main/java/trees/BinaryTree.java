227c319eb866006349309dd61b4114804165180b
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-20 09:23:11 +0300

    add binary tree defination

--- src/main/java/trees/BinaryTree.java
+++ src/main/java/trees/BinaryTree.java
@@ -1,4 +1,12 @@
 package trees;
 
+/*
+*A binary tree is a recursive data structure where each node can have 2 children at most.
+
+A common type of binary tree is a binary search tree, in which every node has a value that is greater than or equal to the node values in the left sub-tree, and less than or equal to the node values in the right sub-tree.
+
+Here's a quick visual representation of this type of binary tree:
+*
+* */
 public class BinaryTree {
 }
