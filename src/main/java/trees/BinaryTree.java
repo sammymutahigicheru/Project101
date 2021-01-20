be36a786a3fe505b7f2f23c2bd3425972c1ea7e8
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-20 09:29:31 +0300

    add new node recursively

--- src/main/java/trees/BinaryTree.java
+++ src/main/java/trees/BinaryTree.java
@@ -10,6 +10,19 @@ Here's a quick visual representation of this type of binary tree:
 * */
 public class BinaryTree {
     Node root;
+    private Node addRecursive(Node current,int value){
+        if (current == null){
+            return new Node(value);
+        }
+        if (value < current.value){
+            current.left = addRecursive(current.left,value);
+        }else if (value > current.value){
+            current.right = addRecursive(current.right,value);
+        }else{
+            return current;
+        }
+        return current;
+    }
 }
 class Node{
     int value;
