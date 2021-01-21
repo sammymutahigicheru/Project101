b8277b696f9ed81cd953ae34503653594e677749
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-21 09:38:02 +0300

    add problem statement

--- src/main/java/challenges/recursionandbacktracking/StairsChallenge.java
+++ src/main/java/challenges/recursionandbacktracking/StairsChallenge.java
@@ -1,4 +1,10 @@
 package challenges.recursionandbacktracking;
 
+/*
+* Davis has a number of staircases in his house and he likes to climb each staircase 1 , 2, or 3 steps at a time. Being a very precocious child, he wonders how many ways there are to reach the top of the staircase.
+
+Given the respective heights for each of the  staircases in his house, find and print the number of ways he can climb each staircase, module  on a new line.
+*
+* */
 public class StairsChallenge {
 }
