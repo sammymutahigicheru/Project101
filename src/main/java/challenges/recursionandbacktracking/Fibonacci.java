935b4be3a687cfddd630d3f4819cf9cb47adddb9
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-21 09:35:25 +0300

    add fibonaccie recursive solution

--- src/main/java/challenges/recursionandbacktracking/Fibonacci.java
+++ src/main/java/challenges/recursionandbacktracking/Fibonacci.java
@@ -12,6 +12,9 @@ Programmatically:
 * */
 class Fibonacci {
     public static int fibonacci(int n){
-
+        if(n <= 1){
+            return n;
+        }
+        return  fibonacci(n-1) + fibonacci(n-2);
     }
 }
