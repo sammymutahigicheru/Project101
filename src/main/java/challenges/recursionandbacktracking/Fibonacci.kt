fad76a7ff3ee62c78a917e8a16bc09343e9f51f4
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-21 09:21:25 +0300

    add problem statement

--- src/main/java/challenges/recursionandbacktracking/Fibonacci.kt
+++ src/main/java/challenges/recursionandbacktracking/Fibonacci.kt
@@ -1,4 +1,14 @@
 package challenges.recursionandbacktracking
 
+/*
+* The Fibonacci Sequence
+
+The Fibonacci sequence appears in nature all around us, in the arrangement of seeds in a sunflower and the spiral of a nautilus for example.
+
+The Fibonacci sequence begins with  and  as its first and second terms. After these first two elements, each subsequent element is equal to the sum of the previous two elements.
+
+Programmatically:
+*
+* */
 class Fibonacci {
 }
