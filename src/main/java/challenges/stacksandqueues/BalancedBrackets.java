028236446942aa7b8be2a994caf2f31f947b56f1
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-22 10:13:24 +0300

    add problem statement

--- src/main/java/challenges/stacksandqueues/BalancedBrackets.java
+++ src/main/java/challenges/stacksandqueues/BalancedBrackets.java
@@ -2,5 +2,18 @@ package challenges.stacksandqueues;
 
 import java.util.Stack;
 
+/*
+* A bracket is considered to be any one of the following characters: (, ), {, }, [, or ].
+
+Two brackets are considered to be a matched pair if the an opening bracket (i.e., (, [, or {) occurs to the left of a closing bracket (i.e., ), ], or }) of the exact same type. There are three types of matched pairs of brackets: [], {}, and ().
+
+A matching pair of brackets is not balanced if the set of brackets it encloses are not matched. For example, {[(])} is not balanced because the contents in between { and } are not balanced. The pair of square brackets encloses a single, unbalanced opening bracket, (, and the pair of parentheses encloses a single, unbalanced closing square bracket, ].
+
+By this logic, we say a sequence of brackets is balanced if the following conditions are met:
+
+It contains no unmatched brackets.
+The subset of brackets enclosed within the confines of a matched pair of brackets is also a matched pair of brackets.
+*
+* */
 public class BalancedBrackets {
 }
