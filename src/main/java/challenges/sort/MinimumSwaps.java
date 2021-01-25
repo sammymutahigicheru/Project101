2e5c9d873ef52c9a5e2f0c355c90e05c7a90d708
Sammy Mutahi <sammymutahi12@gmail.com>
2021-01-25 10:34:25 +0300

    add minimum swaps method

--- src/main/java/challenges/sort/MinimumSwaps.java
+++ src/main/java/challenges/sort/MinimumSwaps.java
@@ -2,8 +2,13 @@ package challenges.sort;
 
 
 /*
-* You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates. You are allowed to swap any two elements. You need to find the minimum number of swaps required to sort the array in ascending order.
+* You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
+* You are allowed to swap any two elements.
+* You need to find the minimum number of swaps required to sort the array in ascending order.
 *
 * */
 public class MinimumSwaps {
+    static int minimumSwaps(int[] arr){
+
+    }
 }
