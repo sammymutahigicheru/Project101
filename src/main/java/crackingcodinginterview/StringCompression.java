7fe4ac9c73dcc8996880ba4f465993efc3c01b32
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-03 13:31:15 +0300

    add all adge cases to the test

--- src/main/java/crackingcodinginterview/StringCompression.java
+++ src/main/java/crackingcodinginterview/StringCompression.java
@@ -2,16 +2,17 @@ package crackingcodinginterview;
 
 /*
 *
-* Compress String eg aabccccdaaa = a2b1cccc4d1a3
+* Compress String eg aabccccdaaa = a2b1c4d1a3
 *
 * */
 public class StringCompression {
-    private StringBuilder stringBuilder = new StringBuilder();
+    private final StringBuilder stringBuilder = new StringBuilder();
     private int count = 1;
     String compressString(String s){
 
-        for (int i=0;i<s.length()-1;i++){
-            if (s.charAt(i) == s.charAt(i+1)){
+        for (int i=0;i<s.length();i++){
+
+            if ( i+1<s.length()&&s.charAt(i) == s.charAt(i+1) ){
                 count++;
             }else{
                 stringBuilder.append(s.charAt(i)).append(count);
--- src/test/crackingcodinginterview/StringCompressionTest.java
+++ src/test/crackingcodinginterview/StringCompressionTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 public class StringCompressionTest {
     private StringCompression stringCompression;
     private String test_string1 = "aabccccaaa";
+    private String test_string2 = "aabcccdaaa";
+    private String fake_test_string = "aabccccdaaa";
     @Before
     public void setUp(){
         stringCompression = new StringCompression();
@@ -15,5 +17,13 @@ public class StringCompressionTest {
     @Test
     public void givenAString_ReturnACompressedStringVersion(){
         Assert.assertEquals("a2b1c4a3",stringCompression.compressString(test_string1));
+    }
+    @Test
+    public void inputString2_ReturnCompressedString(){
+        Assert.assertEquals("a2b1c3d1a3",stringCompression.compressString(test_string2));
+    }
+    @Test
+    public void inputWrongExpectation_ReturnCorrectCompressedString(){
+        Assert.assertNotEquals("a2b1c3d1a3",stringCompression.compressString(fake_test_string));
     }
 }
